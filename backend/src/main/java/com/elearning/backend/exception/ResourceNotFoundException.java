40bd40067f8a383d0c685d6b7122971d555340e8
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-05 04:11:20 +0530

    feat(auth): add resource not found exception

---
+++ backend/src/main/java/com/elearning/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.elearning.backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
