f43b97253b9ed63bbf6668a0093b3ec70f6a0785
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-05 04:21:17 +0530

    feat: add quiz answer repository

---
+++ backend/src/main/java/com/elearning/backend/repository/QuizAnswerRepository.java
@@ -0,0 +1,6 @@
+package com.elearning.backend.repository;
+
+import com.elearning.backend.entity.QuizAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizAnswerRepository extends JpaRepository<QuizAnswer,Long> {}
