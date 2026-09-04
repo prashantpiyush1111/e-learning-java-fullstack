04cf8c787ea6a15815c19c5db780fc3fa5ee3150
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-05 04:12:34 +0530

    feat(auth): add application security constants

---
+++ backend/src/main/java/com/elearning/backend/util/AppConstants.java
@@ -0,0 +1,12 @@
+package com.elearning.backend.util;
+
+public final class AppConstants {
+
+    private AppConstants() {
+    }
+
+    public static final String API_AUTH = "/api/auth";
+    public static final String ROLE_STUDENT = "ROLE_STUDENT";
+    public static final String ROLE_INSTRUCTOR = "ROLE_INSTRUCTOR";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+}
